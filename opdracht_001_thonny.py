5b2a0d76e93ac19b001259bacd2ca9569875b98c
Raimon Leeuwenburg <r.leeuwenburg@gmail.com>
2020-04-21 11:53:44 +0200

    initial commit

---
+++ opdracht_001_thonny.py
@@ -0,0 +1,8 @@
+names = ["Alice", "Bob", "Charlie", "Deirdre"]
+
+for name in names:
+    print("Hi", name)
+    
+a = 11
+b = 31
+print(a+b)
---
+++ opdracht_002_pythondraaien.py
@@ -0,0 +1,5 @@
+print("Hello World!")
+
+a = 11
+b = 31
+print(a+b)
